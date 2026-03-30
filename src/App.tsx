dc0a1eeaf118c7048b17f8cfa5b73d87de8485fb
nelbio <167593673+nelbio@users.noreply.github.com>
2026-03-30 12:25:40 +0100

    commit initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    
+      <div>
+<p>hello</p>
+      </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+                <a href="#"></a>
+        </div>
+    )
+}
+
+export default Navbar
